5192c748b0a09b343792e9c65b86345f980f3805
Ravindra <152324072+ravindrauk@users.noreply.github.com>
2025-03-06 13:23:54 +0000

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,36 +1,8 @@
+
 terraform {
   backend "s3" {
-    bucket = "hotstars3bucket97" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "Myhotstarserverbucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
   }
-}
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "arn:aws:s3:::hotstars3bucket97",
-      "Condition": {
-        "StringEquals": {
-          "s3:prefix": "EKS/terraform.tfstate"
-        }
-      }
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject"],
-      "Resource": [
-        "arn:aws:s3:::EKS/terraform.tfstate"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
-      "Resource": [
-        "arn:aws:s3:::EKS/terraform.tfstate.tflock"
-      ]
-    }
-  ]
 }
